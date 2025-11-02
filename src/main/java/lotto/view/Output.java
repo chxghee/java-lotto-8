8bb395cedf2a049c025aaca6dc537266e08a709c
chxghee <francis001021@gmail.com>
2025-11-02 22:06:25 +0900

    feat(view): 입출력 구현

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+import lotto.model.Lotto;
+import lotto.model.Lottos;
+
+public class Output {
+
+    public static void printPurchaseInputMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printPurchaseAmountMessage(int amount) {
+        System.out.println(amount + "개를 구매했습니다.");
+    }
+
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public static void printLottos(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto.getNumbers());
+        }
+        System.out.println();
+    }
+
+}
